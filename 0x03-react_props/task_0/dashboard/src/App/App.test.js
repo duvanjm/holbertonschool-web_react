c8dd1d6432cc93b92c042af89cccc039a398df15
duvanjm <duvanjarin@gmail.com>
2021-11-06 13:11:06 -0500

    add files

---
+++ 0x03-react_props/task_0/dashboard/src/App/App.test.js
@@ -0,0 +1,25 @@
+/* eslint-disable jest/valid-expect */
+import { shallow } from 'enzyme';
+import { expect } from 'chai';
+import App from './App';
+import Header from '../Header/Header';
+import Login from '../Login/Login';
+import Footer from '../Footer/Footer';
+
+const wrapper = shallow(<App />);
+
+describe('<App />', () => {
+  it('render App component', () => {
+    shallow(<App />);
+    expect(wrapper.exists());
+  });
+  it('renders App-header', () => {
+    expect(wrapper.contains(<Header className="App-header" />));
+  });
+  it('renders App-body', () => {
+    expect(wrapper.contains(<Login className='App-body' />));
+  });
+  it('renders App-footer', () => {
+    expect(wrapper.contains(<Footer className='App-footer' />));
+  });
+});
