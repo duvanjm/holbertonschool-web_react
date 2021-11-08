910081945b2544b471e941d4c8b503d2642f367d
duvanjm <duvanjarin@gmail.com>
2021-11-08 16:07:47 -0500

    add task

---
+++ 0x03-react_props/task_1/dashboard/src/Notifications/Notifications.test.js
@@ -0,0 +1,17 @@
+/* eslint-disable jest/valid-expect */
+import React from 'react';
+import { shallow } from 'enzyme';
+import { expect } from 'chai';
+import Notifications from './Notifications';
+
+const wrapper = shallow(<Notifications />);
+
+describe('Notifications renders without crashing', () => {
+  it('Notifications renders three list items', () => {
+    expect(wrapper.find('ul')).to.have.lengthOf(1);
+    expect(wrapper.find('li')).to.have.lengthOf(3);
+  });
+  it('Notifications renders the text', () => {
+    expect(wrapper.contains(<p>Here is the list of notifications</p>));
+  });
+});
