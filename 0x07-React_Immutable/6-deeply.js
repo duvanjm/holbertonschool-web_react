7f4a745e62ece0f9d95439a417f5328ecb608ab9
duvanjm <duvanjarin@gmail.com>
2021-11-30 16:07:18 -0500

    fix function mergeDeeplyElements

--- 0x07-React_Immutable/6-deeply.js
+++ 0x07-React_Immutable/6-deeply.js
@@ -4,7 +4,7 @@ export default function mergeDeeplyElements(page1, page2) {
   const map1 = Map(page1);
   const map2 = Map(page2);
   if (page1 === page2) {
-    return map1.merge(map2);
+    return map1.mergeDeep(map2);
   }
   return map1.concat(map2);
 }
