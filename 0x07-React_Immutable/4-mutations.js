926e8f59559e71c73b28ffad43f550cd37e145ea
duvanjm <duvanjarin@gmail.com>
2021-11-30 15:26:25 -0500

    fix

--- 0x07-React_Immutable/4-mutations.js
+++ 0x07-React_Immutable/4-mutations.js
@@ -1,16 +1,15 @@
 import { Map, List } from 'immutable';
 
-export const map = () => {
-  Map({
+export const map = Map({
     1: 'Liam',
     2: 'Noah',
     3: 'Elijah',
     4: 'Oliver',
     5: 'Jacob',
     6: 'Lucas',
-  });
-};
+});
 
 export const map2 = List(map).withMutations((list) => {
-  list.set(2, 'Benjamin').set(4, 'Oliver');
+  list.set(2, 'Benjamin');
+  list.set(4, 'Oliver');
 });
