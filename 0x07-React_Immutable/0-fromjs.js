fe3cc92a0ad7daae822a2e6bbf6abc53e766241f
duvanjm <duvanjarin@gmail.com>
2021-11-30 12:06:54 -0500

    add task

--- 0x07-React_Immutable/0-fromjs.js
+++ 0x07-React_Immutable/0-fromjs.js
@@ -4,12 +4,3 @@ function getImmutableObject(object) {
   const map1 = Map(object);
   return map1;
 }
-
-obj = {
-  fear: true,
-  smell: -1033575916.9145899,
-  wall: false,
-  thing: -914767132
-};
-
-console.log(getImmutableObject(obj));
