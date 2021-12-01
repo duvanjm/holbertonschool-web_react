02726c2d7fcb44a932425f7d285da035193b0e19
duvanjm <duvanjarin@gmail.com>
2021-12-01 15:07:34 -0500

    fix uppercase name

--- 0x07-React_Immutable/8-seq.js
+++ 0x07-React_Immutable/8-seq.js
@@ -5,8 +5,8 @@ export default function printBestStudents(obj) {
     .filter((student) => student.score > 70)
     .map((student) => ({
       score: student.score,
-      firstName: student.firstName,
-      lastName: student.lastName,
+      firstName: `${student.firstName.charAt(0).toUpperCase()}${student.firstName.slice(1)}`,
+      lastName: `${student.lastName.charAt(0).toUpperCase()}${student.lastName.slice(1)}`,
     }));
   console.log(score.toObject());
 }
